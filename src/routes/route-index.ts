ec435fbaf107d7ec8d99335b49a7f0bc31ecb27e
Shubham Patel <ptlshubham@gmail.com>
2025-12-04 11:01:50 +0530

    Middleware,Google Login

--- src/routes/route-index.ts
+++ src/routes/route-index.ts
@@ -7,6 +7,7 @@ export default (app: Application) => {
     app.use("/user", require("./api-webapp/authentication/user/user-api"));
     app.use("/company", require("./api-webapp/company/company-api"));
     app.use("/otp", require("./api-webapp/otp/otp-api"));
+    app.use("/login-history", require("./api-webapp/loginHistory/loginHistory-api"));
     app.use("/premiumModule", require("./api-webapp/superAdmin/generalSetup/premiumModule/premiumModule-api"));
     app.use("/category", require("./api-webapp/superAdmin/generalSetup/category/category-api"));
     // app.use("/clients", require("./api-webapp/superAdmin/agency/clients/clients-api"));
